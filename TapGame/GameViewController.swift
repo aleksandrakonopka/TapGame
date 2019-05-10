77b4b1246c3401cec2423701470541036f2ee405
aleksandrakonopka <aleksandrakonopka424@gmail.com>
2019-05-10 16:51:31 +0200

    alert added and time left timer added

--- TapGame/GameViewController.swift
+++ TapGame/GameViewController.swift
@@ -9,21 +9,60 @@
 import UIKit
 
 class GameViewController: UIViewController {
-    var messages = ["3","2","1","0","PLAY!"]
+    var messages = ["3","2","1"/*,"PLAY!"*/]
     var index = 0
     var score = 0
+    var timeClock = 5
+    var timerPlayIndex = 0
+ 
+    @IBOutlet var timeLeftLabel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var hurryUpLabel: UILabel!
     var timer = Timer()
+    var timerClock = Timer()
+    var playTimer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         hurryUpLabel.isHidden = true
+        timeLeftLabel.isHidden = true
         scoreLabel.isHidden = true
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(action), userInfo: nil, repeats: true)
-        var gameTimer = Timer.scheduledTimer(timeInterval: 7, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: false)
+        //var gameTimer = Timer.scheduledTimer(timeInterval: 9, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: false)
         // Do any additional setup after loading the view.
+        
+    }
+    
+    func addAlert()
+    {
+        let alert = UIAlertController(title: "The end", message: "Game has finished", preferredStyle: .alert )
+
+        let ok = UIAlertAction(title: "OK", style: .default){ action in
+           self.dismiss(animated: true, completion: nil)
+           self.dismiss(animated: true, completion: nil)
+                }
+        
+        alert.addAction(ok)
+        self.present(alert,animated: true, completion: nil)
+    }
+
+
+    @objc func moveClock()
+    {
+        timeClock = timeClock - 1
+        if timeClock > -1
+        {
+            timeLeftLabel.text = "Time left: \(timeClock)"
+        }
+        else
+        {
+            timerClock.invalidate()
+            let oneTap = (self.view.gestureRecognizers?.filter(){$0 is UITapGestureRecognizer}.first!)!
+            self.view.removeGestureRecognizer(oneTap)
+            //self.dismiss(animated: true, completion: nil)
+            addAlert()
+        }
         
     }
     @objc func tappedOnce(_ tap : UIGestureRecognizer)
@@ -34,26 +73,43 @@ class GameViewController: UIViewController {
     @objc func action()
     {
         index = index + 1
-        if index < 5
+        if index < 3
         {
         timerLabel.text = messages[index]
         }
         else
         {
             timer.invalidate()
-            timerLabel.isHidden = true
-            hurryUpLabel.isHidden = false
-            scoreLabel.isHidden = false
-            let oneTap = UITapGestureRecognizer(target: self, action: #selector(tappedOnce(_:)))
-            self.view.addGestureRecognizer(oneTap)
+            playTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(playAction), userInfo: nil, repeats: true)
         }
     }
-    @objc func runTimedCode()
+    @objc func playAction()
     {
-        let oneTap = (self.view.gestureRecognizers?.filter(){$0 is UITapGestureRecognizer}.first!)!
-        self.view.removeGestureRecognizer(oneTap)
-        self.dismiss(animated: true, completion: nil)
+        timerPlayIndex = timerPlayIndex + 1
+        if timerPlayIndex < 2
+        {
+            timerLabel.text = "PLAY!"
+        }
+        else
+        {
+        playTimer.invalidate()
+        timerLabel.isHidden = true
+        hurryUpLabel.isHidden = false
+        scoreLabel.isHidden = false
+        timeLeftLabel.isHidden = false
+        let oneTap = UITapGestureRecognizer(target: self, action: #selector(tappedOnce(_:)))
+        self.view.addGestureRecognizer(oneTap)
+        timerClock = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(moveClock), userInfo: nil, repeats: true)
+        }
+
     }
+    
+   // @objc func runTimedCode()
+   // {
+//        let oneTap = (self.view.gestureRecognizers?.filter(){$0 is UITapGestureRecognizer}.first!)!
+//        self.view.removeGestureRecognizer(oneTap)
+//        self.dismiss(animated: true, completion: nil)
+   // }
 
     /*
     // MARK: - Navigation
